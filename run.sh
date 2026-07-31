2ef09f2d29a358caf0be0770a8eb2260d3cb9555
Marco-cefetmg <marcoaureli.o@outlook.com>
2026-07-31 14:30:33 +0000

    disable ' --ignore-requires-python '

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ if [[ -z $1 ]]; then
     docker system prune -a --volumes --force
 fi
 
-for relver in 20.04 22.04 24.04; do
+for relver in 20.04 22.04; do
     if ! docker image inspect "selkies-py-build:latest" >/dev/null 2>&1; then
         for svc in web dist; do 
 	        BUILDX_NO_DEFAULT_ATTESTATIONS=1 BUILDKIT_PARALLEL_LIMIT=1 docker-compose --log-level DEBUG build $svc 2>&1 | tee build_${svc}_$(date +%Y%m%d_%H%M%S).log
         done
     fi
     echo "$relver"
-    BUILDX_NO_DEFAULT_ATTESTATIONS=1 BUILDKIT_PARALLEL_LIMIT=1 DISTRIB_RELEASE=$relver docker-compose --log-level DEBUG up test --build test --force-recreate --remove-orphans 2>&1 | tee up_ubuntu-${relver}_$(date +%Y%m%d_%H%M%S).log
+    BUILDX_NO_DEFAULT_ATTESTATIONS=1 BUILDKIT_PARALLEL_LIMIT=1 DISTRIB_RELEASE=$relver docker-compose --log-level DEBUG up --build test --force-recreate --remove-orphans 2>&1 | tee up_ubuntu-${relver}_$(date +%Y%m%d_%H%M%S).log
 done
