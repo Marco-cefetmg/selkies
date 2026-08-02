c8b429e53d77b9fa240db4fa60c478e58f95575b
Marco-cefetmg <marcoaureli.o@outlook.com>
2026-08-02 16:51:26 -0300

    Update Dockerfile and docker-compose for improved context handling and run script enhancements

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo -S -v -p '' <<<"qwe123"
+
+# [[ "$EUID" == 0 ]] || exec sudo -s "$0" "$@"
+
+# shopt -s expand_aliases
+# alias docker=podman
+# alias docker-compose=podman-compose
 
 if [[ -z $1 ]]; then
     rm -rf *.log
@@ -7,12 +14,12 @@ if [[ -z $1 ]]; then
     docker system prune -a --volumes --force
 fi
 
-for relver in 20.04 22.04; do
-    if ! docker image inspect "selkies-py-build:latest" >/dev/null 2>&1; then
+for relver in 24.04 22.04 20.04; do
+    if docker image inspect "selkies-py-build:latest" >/dev/null 2>&1; then
         for svc in web dist; do 
-	        BUILDX_NO_DEFAULT_ATTESTATIONS=1 BUILDKIT_PARALLEL_LIMIT=1 docker-compose --log-level DEBUG build $svc 2>&1 | tee build_${svc}_$(date +%Y%m%d_%H%M%S).log
+	        BUILDX_NO_DEFAULT_ATTESTATIONS=1 BUILDKIT_PARALLEL_LIMIT=1 docker compose build $svc 2>&1 | tee build_${svc}_$(date +%Y%m%d_%H%M%S).log
         done
     fi
     echo "$relver"
-    BUILDX_NO_DEFAULT_ATTESTATIONS=1 BUILDKIT_PARALLEL_LIMIT=1 DISTRIB_RELEASE=$relver docker-compose --log-level DEBUG up --build test --force-recreate --remove-orphans 2>&1 | tee up_ubuntu-${relver}_$(date +%Y%m%d_%H%M%S).log
+    BUILDX_NO_DEFAULT_ATTESTATIONS=1 BUILDKIT_PARALLEL_LIMIT=1 BUILDAH_FORMAT=docker DISTRIB_RELEASE=$relver docker compose up --build test --force-recreate --remove-orphans 2>&1 | tee up_ubuntu-${relver}_$(date +%Y%m%d_%H%M%S).log
 done
